7511d50b4e8ca6132ca38d741213d38648b321b5
Jeff Bai <j.bai@wustl.edu>
2021-01-20 09:28:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,6 +27,7 @@ class Application
       if @@items.include?(item_to_add)
         @@items << item_to_add
       else
+        "We don't have that item"
       end
     else
       resp.write "Path Not Found"
